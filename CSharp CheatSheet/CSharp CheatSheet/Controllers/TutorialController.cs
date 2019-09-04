4728a3e834d71b39a8fae44807632c4d3ba95dc4
asyrafahmad <are.syraf9764@gmail.com>
2019-09-04 11:40:06 +0800

    1st commit

---
+++ CSharp CheatSheet/CSharp CheatSheet/Controllers/TutorialController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharp_CheatSheet.Controllers
+{
+    public class TutorialController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Introduction()
+        {
+            return View();
+        }
+    }
+}
